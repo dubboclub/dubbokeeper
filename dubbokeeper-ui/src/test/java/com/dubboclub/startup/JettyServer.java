dd6a1cefbdd05f67dfedb97a159950d253bbec07
°ëҹÀÉÖÐ <dubboclub@aliyun.com>
2015-06-16 19:15:33 +0800

    对项目添加jetty启动方式

---
+++ dubbokeeper-ui/src/test/java/com/dubboclub/startup/JettyServer.java
@@ -0,0 +1,30 @@
+package com.dubboclub.startup;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * Created by bieber on 2015/5/26.
+ */
+public class JettyServer {
+    
+    public static void main(String[] args) throws Exception {
+        final Server server = new Server(8088);
+        WebAppContext webAppContext = new WebAppContext("dubbokeeper-ui/src/main/webapp", "/");
+        webAppContext.setMaxFormContentSize(1024*1024*60);
+        server.setHandler(webAppContext);
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                try {
+                    server.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        server.start();
+        server.join();
+    }
+}
