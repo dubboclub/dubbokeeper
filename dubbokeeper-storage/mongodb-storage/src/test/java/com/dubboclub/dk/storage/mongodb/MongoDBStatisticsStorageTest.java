d3a3ead9c2c89a1ac00837348b87111135c5c097
hidehai <hidehai@foxmail.com>
2016-03-01 21:32:12 +0800

    unit test

---
+++ dubbokeeper-storage/mongodb-storage/src/test/java/com/dubboclub/dk/storage/mongodb/MongoDBStatisticsStorageTest.java
@@ -0,0 +1,20 @@
+package com.dubboclub.dk.storage.mongodb;
+
+import com.dubboclub.dk.storage.StatisticsStorage;
+import com.dubboclub.dk.storage.model.Statistics;
+import org.junit.Test;
+
+/**
+ * Created by HideHai on 2016/3/1.
+ */
+public class MongoDBStatisticsStorageTest {
+
+    private StatisticsStorage statisticsStorage;
+
+    /**
+     *
+     */
+    @Test
+    public void storeStatisticsTest() {
+    }
+}
