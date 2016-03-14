c67f2d3d58ea64cdbf224c531b4fe70e5fffb7e8
hidehai <hidehai@foxmail.com>
2016-03-14 22:42:16 +0800

    实现部分接口和本地测试.

--- dubbokeeper-storage/mongodb-storage/src/main/java/com/dubboclub/dk/storage/mongodb/MongoDBStatisticsStorage.java
+++ dubbokeeper-storage/mongodb-storage/src/main/java/com/dubboclub/dk/storage/mongodb/MongoDBStatisticsStorage.java
@@ -14,10 +14,16 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 import org.apache.commons.lang.StringUtils;
 import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -35,6 +41,7 @@ public class MongoDBStatisticsStorage implements StatisticsStorage {
 
     private volatile static MongoDatabase mongoDatabase;
 
+    @Autowired
     private MongoTemplate mongoTemplate;
 
     private static final String APPLICATION_COLLECTIONS = "applications";
@@ -45,17 +52,57 @@ public class MongoDBStatisticsStorage implements StatisticsStorage {
 
     @Override
     public void storeStatistics(Statistics statistics) {
-
+        LOGGER.info(statistics.getApplication());
+        String app = statistics.getApplication();
+        if(app == null){
+            LOGGER.warn("url.application miss!");
+            return;
+        }
+        String collectionName = String.format("%s_%s",STATISTICS_COLLECTIONS,app.toLowerCase());
+        mongoTemplate.save(statistics,collectionName);
     }
 
     @Override
     public List<Statistics> queryStatisticsForMethod(String application, String serviceInterface, String method, long startTime, long endTime) {
-        return null;
+       Query query = new Query(
+         Criteria.where("serviceInterface").is(serviceInterface)
+       );
+        query.addCriteria(Criteria.where("method").is(method));
+        query.addCriteria(Criteria.where("timestamp").gte(startTime).lte(endTime));
+
+        String collectionName = String.format("%s_%s",STATISTICS_COLLECTIONS,application.toLowerCase());
+        List<Statistics>  statisticses = mongoTemplate.find(query,Statistics.class,collectionName);
+        return statisticses;
     }
 
     @Override
     public Collection<MethodMonitorOverview> queryMethodMonitorOverview(String application, String serviceInterface, int methodSize, long startTime, long endTime) {
-        return null;
+        List<TempMethodOveride> methods = findMethodForService(application,serviceInterface);
+
+        Collection<MethodMonitorOverview> methodMonitorOverviews = new ArrayList<MethodMonitorOverview>(methods.size());
+        for(TempMethodOveride method : methods){
+            MethodMonitorOverview methodMonitorOverview = new MethodMonitorOverview();
+            Statistics concurrent= findMethodMaxItemByService("concurrent",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics elapsed= findMethodMaxItemByService("elapsed",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics failure= findMethodMaxItemByService("failureCount",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics input= findMethodMaxItemByService("input",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics kbps= findMethodMaxItemByService("kbps",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics output= findMethodMaxItemByService("output",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics success= findMethodMaxItemByService("successCount",application,serviceInterface,method.getM(),startTime,endTime);
+            Statistics tps= findMethodMaxItemByService("tps",application,serviceInterface,method.getM(),startTime,endTime);
+
+            methodMonitorOverview.setMaxConcurrent(concurrent==null?0:concurrent.getConcurrent());
+            methodMonitorOverview.setMaxElapsed(elapsed==null?0:elapsed.getElapsed());
+            methodMonitorOverview.setMaxFailure(failure==null?0:failure.getFailureCount());
+            methodMonitorOverview.setMaxInput(input==null?0:input.getInput());
+            methodMonitorOverview.setMaxKbps(kbps==null?0:kbps.getKbps());
+            methodMonitorOverview.setMaxOutput(output==null?0:output.getOutput());
+            methodMonitorOverview.setMaxSuccess(success==null?0:success.getSuccessCount());
+            methodMonitorOverview.setMaxTps(tps==null?0:tps.getTps());
+            methodMonitorOverview.setMethod(method.getM());
+            methodMonitorOverviews.add(methodMonitorOverview);
+        }
+        return methodMonitorOverviews;
     }
 
     @Override
@@ -81,5 +128,74 @@ public class MongoDBStatisticsStorage implements StatisticsStorage {
     @Override
     public Collection<ServiceInfo> queryServiceByApp(String application, long start, long end) {
         return null;
+    }
+
+    /**
+     * 查询接口下的方法
+     * @param application
+     * @param serviceInterface
+     * @return
+     */
+    private List<TempMethodOveride> findMethodForService(String application,String serviceInterface){
+        TypedAggregation aggregation =new TypedAggregation(Statistics.class,
+                Aggregation.project("method","serviceInterface"),               //限制结果集包含域
+                Aggregation.match(Criteria.where("serviceInterface").is(serviceInterface)),    //过滤数据
+                Aggregation.group("method").first("method").as("m").count().as("total"), //分组聚合
+                Aggregation.sort(Sort.Direction.DESC,"total")   //数据排序
+        );
+
+        List<TempMethodOveride> methodOverides = mongoTemplate.aggregate(aggregation,
+                String.format("%s_%s",STATISTICS_COLLECTIONS,application.toLowerCase()),
+                TempMethodOveride.class).getMappedResults();
+        return  methodOverides;
+    }
+
+
+    /**
+     * 查询区间倒排数据
+     * @param column
+     * @param application
+     * @param serviceInterface
+     * @param method
+     * @param startTime
+     * @param endTime
+     */
+    private Statistics findMethodMaxItemByService(String column,String application, String serviceInterface, String method, long startTime, long endTime){
+        String sql = "select ${item} from `statistics_${application}` where timestamp>=#{start} \n" +
+                "and timestamp<#{end} and serviceInterface=#{serviceInterface} and method=#{method}\n" +
+                "order by ${item} desc limit 1;";
+
+        Query query = new Query(
+                Criteria.where("serviceInterface").is(serviceInterface)
+        );
+        query.addCriteria(Criteria.where("method").is(method));
+        query.addCriteria(Criteria.where("timestamp").gte(startTime).lte(endTime));
+        query.with(new Sort(Sort.Direction.DESC,column)).limit(1);
+
+        Statistics statisticses = mongoTemplate.findOne(query,Statistics.class,
+                String.format("%s_%s",STATISTICS_COLLECTIONS,application.toLowerCase()));
+
+        return statisticses;
+    }
+
+    private class TempMethodOveride{
+       private  String m;
+        private int total;
+
+        public String getM() {
+            return m;
+        }
+
+        public void setM(String m) {
+            this.m = m;
+        }
+
+        public int getTotal() {
+            return total;
+        }
+
+        public void setTotal(int total) {
+            this.total = total;
+        }
     }
 }
--- dubbokeeper-storage/mongodb-storage/src/test/java/com/dubboclub/dk/storage/mongodb/MongoDBStatisticsStorageTest.java
+++ dubbokeeper-storage/mongodb-storage/src/test/java/com/dubboclub/dk/storage/mongodb/MongoDBStatisticsStorageTest.java
@@ -1,20 +1,159 @@
 package com.dubboclub.dk.storage.mongodb;
 
 import com.dubboclub.dk.storage.StatisticsStorage;
+import com.dubboclub.dk.storage.model.MethodMonitorOverview;
 import com.dubboclub.dk.storage.model.Statistics;
+import org.apache.commons.lang.time.DateUtils;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
+import org.springframework.data.mongodb.core.mapreduce.GroupBy;
+import org.springframework.data.mongodb.core.mapreduce.GroupByResults;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by HideHai on 2016/3/1.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:/META-INF/spring/mongodb.xml" })
 public class MongoDBStatisticsStorageTest {
 
+    Logger LOGGER = LoggerFactory.getLogger("dubbokeeper-server");
+
+    @Autowired
     private StatisticsStorage statisticsStorage;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private static final String APPLICATION_COLLECTIONS = "applications";
+
+    private static final String STATISTICS_COLLECTIONS = "statistics";
 
     /**
      *
      */
     @Test
     public void storeStatisticsTest() {
+        Statistics statistics = new Statistics();
+        statistics.setApplication("test_hh_service");
+        statistics.setConcurrent(2);
+        statistics.setElapsed(1);
+        statistics.setFailureCount(0);
+        statistics.setHost("10.100.152.111");
+        statistics.setInput(100);
+        statistics.setOutput(200);
+        statistics.setKbps(300);
+        statistics.setMethod("fetchData");
+        statistics.setRemoteAddress("10.100.152.200");
+        statistics.setRemoteType(Statistics.ApplicationType.PROVIDER);
+        statistics.setServiceInterface("com.hidehai.dubbo.HHSerivce");
+        statistics.setTimestamp(new Date().getTime());
+        statistics.setTps(150);
+
+        statisticsStorage.storeStatistics(statistics);
+    }
+
+    @Test
+    public void queryStatisticsForMethodTest(){
+        String appName = "test_hh_service";
+        String interfaceName = "com.hidehai.dubbo.HHSerivce";
+        String methodName = "fetchData";
+
+        List<Statistics> statisticsList = statisticsStorage.queryStatisticsForMethod(appName,interfaceName,methodName,
+                DateUtils.addHours(new Date(),-5).getTime(),new Date().getTime());
+        if(statisticsList != null){
+            for(Statistics s :statisticsList){
+                LOGGER.info(s.getServiceInterface()+" "+s.getHost());
+            }
+        }
+    }
+
+    @Test
+    public void findMethodForServiceTest(){
+        String serviceInterface = "com.hidehai.dubbo.HHSerivce";
+        String colName = "statistics_test_hh_service";
+
+        //Aggregation条件之间存在顺序关系
+        TypedAggregation aggregation =new TypedAggregation(Statistics.class,
+                Aggregation.project("method","serviceInterface"),               //限制结果集包含域
+                Aggregation.match(Criteria.where("serviceInterface").is(serviceInterface)),    //过滤数据
+                Aggregation.group("method").first("method").as("m").count().as("total"), //分组聚合
+                Aggregation.sort(Sort.Direction.DESC,"total")   //数据排序
+        );
+
+        List<Temp> statisticses = mongoTemplate.aggregate(aggregation,colName,Temp.class).getMappedResults();
+        LOGGER.info(statisticses.size()+"");
+        for (Temp s : statisticses){
+            LOGGER.info(String.format("method: %s ; count: %s",s.getM(),s.getTotal()));
+        }
+    }
+
+    @Test
+    public void findMethodMaxItemByServiceTest(){
+        String serviceInterface = "com.hidehai.dubbo.HHSerivce";
+        String colName = "concurrent";
+        String method = "fetchData2";
+        String application = "test_hh_service";
+
+        Query query = new Query(
+                Criteria.where("serviceInterface").is(serviceInterface)
+        );
+        query.addCriteria(Criteria.where("method").is(method));
+        query.addCriteria(Criteria.where("timestamp").gte(DateUtils.addHours(new Date(),-5).getTime()).lte(new Date().getTime()));
+        query.with(new Sort(Sort.Direction.DESC,colName)).limit(1);
+
+        Statistics statisticses = mongoTemplate.findOne(query,Statistics.class,
+                String.format("%s_%s",STATISTICS_COLLECTIONS,application.toLowerCase()));
+        if(statisticses != null){
+            LOGGER.info(statisticses.getConcurrent()+"");
+        }
+    }
+
+    @Test
+    public void queryMethodMonitorOverviewTest(){
+        String serviceInterface = "com.hidehai.dubbo.HHSerivce";
+        String application = "test_hh_service";
+        Collection<MethodMonitorOverview> overviews = statisticsStorage.queryMethodMonitorOverview(application,
+                serviceInterface,5,DateUtils.addHours(new Date(),-5).getTime(),new Date().getTime());
+
+        for(MethodMonitorOverview m : overviews){
+            LOGGER.info(
+                    String.format("method: %s - Concurrent:%s - kbps:%s",m.getMethod(),m.getMaxConcurrent(),m.getMaxKbps()));
+        }
+    }
+
+    class Temp{
+        String m;
+        int total;
+
+        public String getM() {
+            return m;
+        }
+
+        public void setM(String m) {
+            this.m = m;
+        }
+
+        public int getTotal() {
+            return total;
+        }
+
+        public void setTotal(int total) {
+            this.total = total;
+        }
     }
 }
