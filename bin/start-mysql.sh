3a35ea24f648dc5da26467813ae6774748c1db6b
bieber <bieber@NetRestoreofMacintoshHD-98e0d97c5c0d.local>
2016-03-24 22:42:42 +0800

    调整shell启动脚本

--- bin/start-mysql.sh
+++ bin/start-mysql.sh
@@ -5,11 +5,10 @@ LOG_HOME="${DK_HOME}/../logs"
 CLASSPATH="${DK_HOME}/../lib/*:${CONFDIR}"
 MAINCLASS="com.dubboclub.dk.server.Main"
 
-if [ "$JAVA_HOME" !="" ]; then
-	
-	JAVA="$JAVA_HOME/bin/java"
-else
+if [ -z $JAVA_HOME ]; then
 	JAVA=java
+else
+	JAVA="$JAVA_HOME/bin/java"
 fi
 
 DUBBO_PROPERTIES="dubbo-mysql.properties"
