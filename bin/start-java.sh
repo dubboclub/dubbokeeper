7195bfde77d3b6481a87e39f24de7cbe75d3ca20
yong.ma <maywide@qq.com>
2022-01-03 11:54:03 +0800

    fix start-java.sh for docker environment

--- bin/start-java.sh
+++ bin/start-java.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 cd `dirname $0`
-DK_HOME=`pwd`
-CONFDIR="${DK_HOME}/../conf"
-LOG_HOME="${DK_HOME}/../logs"
+DK_HOME="`pwd`/.."
+CONFDIR="${DK_HOME}/conf"
+LOG_HOME="${DK_HOME}/logs"
 
-BOOT_JAR="`ls ${DK_HOME}/../lib/$1`"
+BOOT_JAR="`ls ${DK_HOME}/lib/$1`"
 JAR_NAME="`basename ${BOOT_JAR}`"
 DUBBO_PROPERTIES="$2"
-
-LOG_FILE="${LOG_HOME}/${JAR_NAME%.*}.out"
-MAX_RETRIES=20
 
 if [ -z $JAVA_HOME ]; then
 	JAVA=java
@@ -17,25 +14,11 @@ else
 	JAVA="$JAVA_HOME/bin/java"
 fi
 
+mkdir -p $LOG_HOME
+
 echo "Start $JAR_NAME ..."
-nohup $JAVA \
+$JAVA \
   -Dmonitor.log.home="${LOG_HOME}" \
   -DDUBBO_LOG_HOME="${LOG_HOME}" \
   -Ddubbo.properties.file="${CONFDIR}/${DUBBO_PROPERTIES}" \
-  -jar ${BOOT_JAR} > "$LOG_FILE" 2>&1 &
-
-echo "LOG_HOME: $LOG_HOME"
-PID=""
-CNT=0
-while [ -z "$PID" -a $CNT -lt $MAX_RETRIES ]; do
-  CNT=$((CNT+1))
-  echo -e ".\c"
-  sleep 1
-  PID="`jps | grep $JAR_NAME | awk '{print $1}'`"
-done
-
-if [ -n "$PID" ]; then
-  echo "OK! $JAR_NAME STARTED. PID: $PID"
-else
-  echo "FAILED to start $JAR_NAME. Please check $LOG_FILE"
-fi
+  -jar ${BOOT_JAR}
